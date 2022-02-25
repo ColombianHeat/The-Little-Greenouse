d9943ecb7eda24241aab6c8157a88e2e335c05a9
Alan Gilchrist <gilchristalanv@gmail.com>
2022-02-24 21:21:18 -0500

    Updated the fan and grow light to operate on independent timers.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include <Adafruit_Sensor.h>
 #include "DHT.h"
 #include <LiquidCrystal.h>
+
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
 #define DHTTYPE DHT11   // DHT 11
 #define SensorPin A1 
 #define sensorPower 7
 #define sensorPin A0
+
 float sensorValue = 0; 
 int val = 0;
 DHT dht(DHTPIN, DHTTYPE);
@@ -16,7 +18,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // constants used for operation of the water pump
 int pumpPin = 13;
-const long wateringInterval = 6000; //how long to wait until checking whether or not to water (default 12 hours or 43,200,000 ms)
+const long wateringInterval = 43200000; //how long to wait until checking whether or not to water (default 12 hours or 43,200,000 ms)
 int pumpActivationLength = 6000; // when activated, the pump will turn on for this much time
 int pumpState = LOW; // determines whether pump is on or off
 int lowMoisture = 630; // below this threshold, the pump will activate when it reaches the end of the watering interval
@@ -25,16 +27,16 @@ unsigned long previousWaterMillis = 0; // stores how long it's been since checki
 
 // constants used for operation of the grow light
 int lightPin = 10;
-const long lightInterval = 86400000; // how long to wait until turning on the light (default 24 hours or 86,400,000 ms)
-const long lightActivationLength = 36000000; // how long to leave the light on (default 10 hours or 36,000,000 ms)
-int lightState = LOW; // determines whether the light is on or off
+const long lightInterval = 86400000; // Period of the grow light cycle (default 24 hours or 86,400,000 ms)
+const long lightActivationLength = 36000000; // how long to leave the light on per period (default 10 hours or 36,000,000 ms)
+int lightState = HIGH; // determines whether the light is on or off
 unsigned long previousLightMillis = 0; // used to keep time, which will determine when the light will turn on/off
 
 // constants used for operation of the fan
 int fanPin = 9;
-const long fanInterval = 7200000; // how long to wait until starting the fan (default 120 minutes or 7,200,000 ms)
-const long fanActivationLength = 3600000; // how long to run the fan for (default 60 minutes or 3,600,000 ms)
-int fanState = LOW; // determines whether fan is on or off
+const long fanInterval = 7200000; // Period of the fan cycle (default 120 minutes or 7,200,000 ms)
+const long fanActivationLength = 3600000; // how long to leave the fan on per period (default 60 minutes or 3,600,000 ms)
+int fanState = HIGH; // determines whether fan is on or off
 unsigned long previousFanMillis = 0; // used to keep time, which will determine when the fan will turn on/off
 
 
@@ -48,7 +50,6 @@ int readSensor() {
 
 void waterPump(){  // this function determines when to turn on the water pump
   if (millis() - previousWaterMillis >= wateringInterval) {
-    Serial.println(millis());
     //if (sensorValue<=lowMoisture) {
       previousWaterMillis = millis();
       
@@ -67,32 +68,35 @@ void waterPump(){  // this function determines when to turn on the water pump
   }
 }
 
+// note to self: the light will be OFF for the first full light cycle, then it will begin to cycle properly
 void growLight() {  // this function determines when to turn on/off the grow light
-    if (millis() - previousLightMillis >= lightInterval) {
-    //Serial.println(millis());
-      previousLightMillis = millis();
-      
-    if (lightState == LOW) {
+  if (millis() - previousLightMillis >= lightInterval && lightState == LOW) {
+    previousLightMillis = millis();
     lightState = HIGH;         // turn light on if it was off
-    }
-    else if (lightState == HIGH) {
-    lightState = LOW;          // turn light off if it was on
-    }
+    Serial.print("GROW LIGHT ON: ");
+    Serial.println(millis());
+    digitalWrite(lightPin, lightState);
+  }
+  else if (millis() - previousLightMillis >= lightActivationLength && lightState == HIGH) {
+    lightState = LOW; 
+    Serial.print("GROW LIGHT OFF: ");
+    Serial.println(millis());
     digitalWrite(lightPin, lightState);
   }
 }
 
 void fan(){  // this function determines when to turn on/off the fan
-  if (millis() - previousFanMillis >= fanInterval) {
-    //Serial.println(millis());
-      previousWaterMillis = millis();
-      
-    if (fanState == LOW) {
-    fanState = HIGH;         // turn fan on if it was off
-    }
-    else if (fanState == HIGH) {
-    fanState = LOW;          // turn fan off if it was on
-    }
+  if (millis() - previousFanMillis >= fanInterval && fanState == LOW) {
+    previousFanMillis = millis();
+    fanState = HIGH;         // turn light on if it was off
+    Serial.print("FAN ON: ");
+    Serial.println(millis());
+    digitalWrite(fanPin, fanState);
+  }
+  else if (millis() - previousFanMillis >= fanActivationLength && fanState == HIGH) {
+    fanState = LOW; 
+    Serial.print("FAN OFF: ");
+    Serial.println(millis());
     digitalWrite(fanPin, fanState);
   }
 }
@@ -102,18 +106,14 @@ void setup() {
   Serial.println(F("Temperature & Humidity + Moisture Level + Water Level Data"));
   dht.begin();
   
-  Serial.begin(9600); 
   pinMode(sensorPower, OUTPUT);
-  pinMode(pumpPin,OUTPUT);
+  pinMode(pumpPin, OUTPUT);
+  pinMode(lightPin, OUTPUT);
+  pinMode(fanPin, OUTPUT);
   // Set to LOW so no power flows through the sensor
   digitalWrite(sensorPower, LOW);
-  
-  Serial.begin(9600);
 
-  
-  Serial.begin(9600);
-
-lcd.begin(16, 2);
+  lcd.begin(16, 2);
 }
 
 void loop() {
@@ -144,7 +144,7 @@ void loop() {
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println(F("Failed to read from DHT sensor!"));
-    return;
+    //return;
   }
 
   // Compute heat index in Fahrenheit (the default)
@@ -163,8 +163,6 @@ void loop() {
   Serial.print(F("C "));
   Serial.print(hif);
   Serial.println(F("F"));
-  Serial.println();
-  Serial.println();
 
   
 lcd.clear();
@@ -173,18 +171,18 @@ lcd.clear();
    lcd.setCursor(0,1);//set the cursor on the second row and first column
    lcd.print((float)dht.readTemperature(),0);//print the temperature
    lcd.print("C");
-   delay(3000);
+   delay(2000);
    lcd.clear();
    lcd.print("Humidity:");
    lcd.setCursor(0,1);//set the cursor on the second row and first column
    lcd.print((float)dht.readHumidity(),0);//print the humidity
    lcd.print("%");
-   delay(3000);
+   delay(2000);
    lcd.clear();
    lcd.print("Moisture level:");
       lcd.setCursor(0,1);//set the cursor on the second row and first column
 lcd.print((sensorValue),0);
-   delay(3000);
+   delay(2000);
    lcd.clear();
       lcd.print("Water level:");
          lcd.setCursor(0,1);//set the cursor on the second row and first column
@@ -192,8 +190,9 @@ lcd.print(level);
    delay(1000);
    lcd.clear();
 
-waterPump(); // calls for water pump function which will activate the pump when needed 
-growLight(); // calls for grow light function
-fan();       // calls for fan function
+  waterPump(); // calls for water pump function which will activate the pump when needed 
+  growLight(); // calls for grow light function
+  fan();       // calls for fan function
+  Serial.println();
    
 }
