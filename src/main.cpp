9b6773e76f79fd4301337132ad25ae677385218a
Alan Gilchrist <gilchristalanv@gmail.com>
2022-02-28 12:42:49 -0500

    Added capability to change the watering, lighting, and fanning periods based off different preset plant modes.

--- src/main.cpp
+++ src/main.cpp
@@ -16,26 +16,29 @@ DHT dht(DHTPIN, DHTTYPE);
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+const char *plantModeArray[] = {"Tomato", "Cherry", "Grape"};
+int plantMode = 0;
+
 // constants used for operation of the water pump
-int pumpPin = 13;
-const long wateringInterval = 43200000; //how long to wait until checking whether or not to water (default 12 hours or 43,200,000 ms)
-int pumpActivationLength = 6000; // when activated, the pump will turn on for this much time
+const int pumpPin = 13;
+unsigned long wateringInterval = 10000; //how long to wait until checking whether or not to water (default 12 hours or 43,200,000 ms)
+unsigned long pumpActivationLength = 6000; // when activated, the pump will turn on for this much time
 int pumpState = LOW; // determines whether pump is on or off
 int lowMoisture = 630; // below this threshold, the pump will activate when it reaches the end of the watering interval
 //unsigned long currentWaterMillis = millis();
 unsigned long previousWaterMillis = 0; // stores how long it's been since checking moisture levels for watering purposes
 
 // constants used for operation of the grow light
-int lightPin = 10;
-const long lightInterval = 86400000; // Period of the grow light cycle (default 24 hours or 86,400,000 ms)
-const long lightActivationLength = 36000000; // how long to leave the light on per period (default 10 hours or 36,000,000 ms)
+const int lightPin = 10;
+unsigned long lightInterval = 24000; // Period of the grow light cycle (default 24 hours or 86,400,000 ms)
+unsigned long lightActivationLength = 8000; // how long to leave the light on per period (default 10 hours or 36,000,000 ms)
 int lightState = HIGH; // determines whether the light is on or off
 unsigned long previousLightMillis = 0; // used to keep time, which will determine when the light will turn on/off
 
 // constants used for operation of the fan
-int fanPin = 9;
-const long fanInterval = 7200000; // Period of the fan cycle (default 120 minutes or 7,200,000 ms)
-const long fanActivationLength = 3600000; // how long to leave the fan on per period (default 60 minutes or 3,600,000 ms)
+const int fanPin = 9;
+unsigned long fanInterval = 12000; // Period of the fan cycle (default 120 minutes or 7,200,000 ms)
+unsigned long fanActivationLength = 6000; // how long to leave the fan on per period (default 60 minutes or 3,600,000 ms)
 int fanState = HIGH; // determines whether fan is on or off
 unsigned long previousFanMillis = 0; // used to keep time, which will determine when the fan will turn on/off
 
@@ -56,12 +59,15 @@ void waterPump(){  // this function determines when to turn on the water pump
       if (pumpState == LOW) {
       pumpState = HIGH;         // turn pump on if soil moisture is below threshold
       }
-
+  Serial.print("PUMP ON: ");
+  Serial.println(millis());
   digitalWrite(pumpPin, pumpState);
   delay(pumpActivationLength);  // if pump was turned on by the function, wait and turn it off
 
   if (pumpState == HIGH) {
     pumpState = LOW;
+    Serial.print("PUMP OFF: ");
+    Serial.println(millis());
     digitalWrite(pumpPin, pumpState);
   }
       //}
@@ -117,12 +123,51 @@ void setup() {
 }
 
 void loop() {
+  if (strcmp(plantModeArray[plantMode], "Tomato") == 0) {
+    Serial.println("TOMATO MODE ENGAGED");
+    wateringInterval = 10000;
+    pumpActivationLength = 6000;
+    lowMoisture = 630; // Maybe this can be the same for all modes??
+
+    lightInterval = 24000;
+    lightActivationLength = 6000;
+
+    fanInterval = 12000;
+    fanActivationLength = 6000;
+  }
+  else if (strcmp(plantModeArray[plantMode], "Cherry") == 0) {
+    Serial.println("CHERRY MODE ENGAGED");
+    wateringInterval = 10000;
+    pumpActivationLength = 6000;
+    lowMoisture = 630; // Maybe this can be the same for all modes??
+
+    lightInterval = 24000;
+    lightActivationLength = 6000;
+
+    fanInterval = 12000;
+    fanActivationLength = 6000;
+  }
+  else if (strcmp(plantModeArray[plantMode], "Grape") == 0) {
+    Serial.println("GRAPE MODE ENGAGED");
+    wateringInterval = 10000;
+    pumpActivationLength = 6000;
+    lowMoisture = 630; // Maybe this can be the same for all modes??
+
+    lightInterval = 24000;
+    lightActivationLength = 6000;
+
+    fanInterval = 12000;
+    fanActivationLength = 6000;
+  }
+  else {
+    Serial.println("No valid plant mode selected!");
+  }
+
   for (int i = 0; i <= 100; i++) 
   { 
    sensorValue = sensorValue + analogRead(SensorPin); 
    delay(1); 
   } 
- delay(1000); 
  sensorValue = sensorValue/100.0; 
  Serial.print("Moisture level: ");
  Serial.println(sensorValue); 
@@ -171,18 +216,18 @@ lcd.clear();
    lcd.setCursor(0,1);//set the cursor on the second row and first column
    lcd.print((float)dht.readTemperature(),0);//print the temperature
    lcd.print("C");
-   delay(2000);
+   delay(1000);
    lcd.clear();
    lcd.print("Humidity:");
    lcd.setCursor(0,1);//set the cursor on the second row and first column
    lcd.print((float)dht.readHumidity(),0);//print the humidity
    lcd.print("%");
-   delay(2000);
+   delay(1000);
    lcd.clear();
    lcd.print("Moisture level:");
       lcd.setCursor(0,1);//set the cursor on the second row and first column
 lcd.print((sensorValue),0);
-   delay(2000);
+   delay(1000);
    lcd.clear();
       lcd.print("Water level:");
          lcd.setCursor(0,1);//set the cursor on the second row and first column
